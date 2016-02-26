2bfb8bda74a395dc82205b7e6f25455548db17b5
binaryk <lupacescueduard@yahoo.com>
2016-02-26 23:02:32 +0200

    CurrentWork

--- config/destinations.php
+++ config/destinations.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'in1' => public_path() . '/files/controller.txt',
-    'out1' => public_path() .DIRECTORY_SEPARATOR. 'out' . DIRECTORY_SEPARATOR,///var/www/html/application/controllers/file.php
-    'in2' => public_path() . DIRECTORY_SEPARATOR . 'files/view.php',
-    'out2' => public_path() . DIRECTORY_SEPARATOR. 'out' . DIRECTORY_SEPARATOR,////var/www/html/application/views/pages/file.php
-    'new_folder' => public_path() . DIRECTORY_SEPARATOR. 'out'.DIRECTORY_SEPARATOR,
-    'sample' => public_path() . DIRECTORY_SEPARATOR .'out'.DIRECTORY_SEPARATOR.'sample',///var/www/html/assets/img/sample
-    'photos_dest' => public_path() . DIRECTORY_SEPARATOR . 'out'.DIRECTORY_SEPARATOR.'assets'.DIRECTORY_SEPARATOR,////var/www/html/assets/img/catlook
+    'in1' =>         public_path() . '/files/controller.txt',
+    'out1' =>        '/var/www/html/application/controllers/',public_path() .DIRECTORY_SEPARATOR. 'out' . DIRECTORY_SEPARATOR,///var/www/html/application/controllers/file.php
+    'in2' =>         public_path() . DIRECTORY_SEPARATOR . 'files/view.php',
+    'out2' =>        '/var/www/html/application/views/pages/', //public_path() . DIRECTORY_SEPARATOR. 'out' . DIRECTORY_SEPARATOR,////var/www/html/application/views/pages/file.php
+    'new_folder' =>  '/var/www/html/ro/uploads/',//public_path() . DIRECTORY_SEPARATOR. 'out'.DIRECTORY_SEPARATOR,
+    'sample' =>      '/var/www/html/assets/img/sample',//public_path() . DIRECTORY_SEPARATOR .'out'.DIRECTORY_SEPARATOR.'sample',///var/www/html/assets/img/sample
+    'photos_dest' => '/var/www/html/assets/img/',//public_path() . DIRECTORY_SEPARATOR . 'out'.DIRECTORY_SEPARATOR.'assets'.DIRECTORY_SEPARATOR,////var/www/html/assets/img/catlook
 ];
