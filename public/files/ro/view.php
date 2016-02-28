d55a37ff03e72c598047e358473bd74346b26314
binaryk <lupacescueduard@yahoo.com>
2016-02-28 17:34:27 +0200

    CurrentWork

--- public/files/ro/view.php
+++ public/files/ro/view.php
@@ -73,7 +73,7 @@ if (isset($_GET['step']) && $_GET['step'] == 1) {
     Nu esti multumit de rezultat? Incearca din nou
 </a>
 </div>
-<div class="result-image-description">E?ti o pisic? atât de adorabil?! To?i trebuie s? vad? asta!!! Împ?rt??e?te acest rezultat cu prietenii, las?-i s? ?tie asta!</div>
+<div class="result-image-description">[[MESSAGE]]</div>
 <p style="margin: 10px 0 0">
     <small>
         <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FQuizOcean-968665643215083&amp;locale=en_GB&amp;width=130&amp;height=20&amp;colorscheme=light&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;send=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden;
